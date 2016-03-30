34749f2597c67d81f5683897b8c73ca456291178
Akhil Pandey <akhilpandey10@gmail.com>
2016-03-30 15:22:52 -0400

    Added comments
    Added comments and SHA-1

--- cachematrix.R
+++ cachematrix.R
@@ -4,13 +4,22 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+	## Initializing m
 	m<- NULL
+	
+	## defining set function
 	set<-function(y) {
 		x<<-y
             m<<-NULL
 	}
+	
+	## defining get function
 	get<-function() x
+	
+	## defining setsolve function
 	setsolve<-function(solve) m <<- solve
+	
+	## defining getsolve function
 	getsolve<-function() m
 	list(set=set, get=get,
 		setmean=setmean,
@@ -22,14 +31,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ## Assumption: Matrix is invertible
 	m<-x$getsolve()
 	if(!is.null(m)) {
 		message("getting cached data")
 		return(m)
 	}
 	data <- x$get()
+	## Computing the inverse of a square matrix can be done with the solve function in R. 
+        ## For example, if X is a square invertible matrix, then solve(X) returns its inverse.
 	m<-solve(data, ...)
 	x$setsolve(m)
+	# Return the inverse
 	m
 }
 }
